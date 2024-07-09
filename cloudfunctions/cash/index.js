5012415c7615eb0e98baaa693c81a5603a7db25e
tsukasateima <tsukasa4833@gmail.com>
2024-07-09 13:07:32 +0800

    build:项目初始化基建

---
+++ cloudfunctions/cash/index.js
@@ -0,0 +1,66 @@
+var config = {
+      appid: 'wxda1f9e7de1b054cc', //小程序Appid
+      envName: 'cloud1-4gweu4m89ff0f881', // 小程序云开发环境ID
+      mchid: '158xxxxxx', //商户号
+      partnerKey: 'xxxxxxxxxxxxxxxxxx', //此处填服务商密钥32位
+      pfx: '', //证书初始化
+      fileID: 'cloud://shixianxxxxxxxxxxxxx/apiclient_cert.p12', //证书云存储id
+      actionName: '安农大闲置书籍小程序提现',
+      rate: 1 //提现收取利率，1指的是每笔收取1%
+};
+
+/*
+下
+面
+不
+用
+管
+*/
+var cloud = require('wx-server-sdk')
+cloud.init({
+      env: config.envName
+})
+
+var db = cloud.database();
+var tenpay = require('tenpay'); //支付核心模块
+exports.main = async (event, context) => {
+
+      var userInfo = (await db.collection('user').doc(event.userid).get()).data;
+      //     if (userInfo.parse <= Number(event.num)){
+      //           return 0;
+      //     }
+      //首先获取证书文件
+      var fileres = await cloud.downloadFile({
+            fileID: config.fileID,
+      })
+      config.pfx = fileres.fileContent
+      var pay = new tenpay(config, true)
+      var result = await pay.transfers({
+            partner_trade_no: 'bookcash' + Date.now() + Number(event.num),
+            openid: userInfo._openid,
+            check_name: 'NO_CHECK',
+            amount: Number(event.num) - 1,
+            desc: config.actionName,
+      });
+      console.log(result);
+      if (result.result_code == 'SUCCESS') {
+            //成功后操作
+            //以下是进行余额计算
+            try {
+                  return await db.collection('user').doc(event.userid).update({
+                        data: {
+                              parse: userInfo.parse - Number(event.num)
+                        }
+                  })
+            } catch (e) {
+                  console.log(e)
+            }
+            // var res = await db.collection('user').doc(event.userid).update({
+            //       data: {
+            //             parse: Number(userInfo.parse) - Number(event.num)
+            //       }
+            // });
+            // console.log(res);
+            // return res
+      }
+}
